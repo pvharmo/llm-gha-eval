a1cd319dfa3f96ae4b143ebf0640a20887955388
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2024-05-14 16:51:28 -0400

    Add dataset builder

--- menu.py
+++ menu.py
@@ -3,3 +3,4 @@ import streamlit as st
 def menu():
     st.sidebar.page_link("pages/Run_LLM_benchmark.py", label="🚀 Run LLM benchmarks")
     st.sidebar.page_link("pages/Visualize_benchmarks_results.py", label="📊 Visualize benchmarks")
+    st.sidebar.page_link("pages/Build_dataset.py", label="📑 Build dataset")
--- pages/Visualize_benchmarks_results.py
+++ pages/Visualize_benchmarks_results.py
@@ -17,38 +17,39 @@ st.title("📊 Visualize benchmarks")
 
 files = [f[:-5] for f in os.listdir("./results") if isfile(join("./results", f)) and f.endswith(".json")]
 
-model_name = st.selectbox("Select a model", files)
+if len(files) > 0:
+    model_name = st.selectbox("Select a model", files)
 
-results = json.loads(open(f"results/{model_name}.json").read())
+    results = json.loads(open(f"results/{model_name}.json").read())
 
-total_success = 0
-count = 0
-for result in results:
-    count += 1
-    for test_result in result["test results"]:
-        if test_result["valid"] == True:
-            total_success += 1
-            break
+    total_success = 0
+    count = 0
+    for result in results:
+        count += 1
+        for test_result in result["test results"]:
+            if test_result["valid"] == True:
+                total_success += 1
+                break
 
-print(total_success / count)
+    print(total_success / count)
 
-st.write("## Results")
+    st.write("## Results")
 
-for result in results:
-    st.write("### Prompt")
-    st.write(result["prompt"])
-    st.write("### System Prompt")
-    st.write(result["system_prompt"])
-    st.write("### Response")
-    with st.expander("Show"):
-        st.write(result["response"])
-    st.write("### Test Results")
-    for test_result in result["test results"]:
-        st.write("#### Valid")
-        st.write(test_result["valid"])
-        st.write("#### Analysis output")
+    for result in results:
+        st.write("### Prompt")
+        st.write(result["prompt"])
+        st.write("### System Prompt")
+        st.write(result["system_prompt"])
+        st.write("### Response")
         with st.expander("Show"):
-            st.code(test_result["result_output"])
-        st.write("#### Generated code")
-        with st.expander("Show"):
-            st.code(test_result["code"], language="yaml")
+            st.write(result["response"])
+        st.write("### Test Results")
+        for test_result in result["test results"]:
+            st.write("#### Valid")
+            st.write(test_result["valid"])
+            st.write("#### Analysis output")
+            with st.expander("Show"):
+                st.code(test_result["result_output"])
+            st.write("#### Generated code")
+            with st.expander("Show"):
+                st.code(test_result["code"], language="yaml")
