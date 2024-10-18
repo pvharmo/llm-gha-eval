810f58325fab8f812d5f0819f21ed94696705610
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2024-10-17 20:36:21 -0400

    set output dir for checkpoints in finetune

--- finetuning/finetune.py
+++ finetuning/finetune.py
@@ -72,7 +72,7 @@ training_config = {
     "lr_scheduler_type": "cosine",
     "num_train_epochs": 1,
     "max_steps": -1,
-    "output_dir": "./checkpoint_dir",
+    "output_dir": "./checkpoints",
     "overwrite_output_dir": True,
     "per_device_eval_batch_size": 4,
     "per_device_train_batch_size": 4,
