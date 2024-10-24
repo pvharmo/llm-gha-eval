73b3bdd3713403782582793db2867064044ac755
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2024-10-24 14:23:31 -0400

    change output file name for inference bash scripts

--- scripts/qwen1.5b_inference.sh
+++ scripts/qwen1.5b_inference.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-6:00
-#SBATCH --output=%N-%j.out
+#SBATCH --output=qwen1.5b_%N-%j.out
 
 module load python/3.12.4 scipy-stack gcc arrow/17.0.0 cuda cudnn
 
