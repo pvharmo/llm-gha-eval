64401bae7a13f4bb11ad5d0cc8799c3d5fc449ac
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2024-10-25 12:20:40 -0400

    fix vllm inference

--- eval/inference_vllm.py
+++ eval/inference_vllm.py
@@ -14,6 +14,9 @@ import env
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", type=str)
+parser.add_argument("--top-p", type=float, default=0.8)
+parser.add_argument("-t", type=float, default=0.7)
+parser.add_argument("--cpu-offload-gb", type=float, default=10)
 parser.add_argument("--finetune", action=argparse.BooleanOptionalAction)
 args = parser.parse_args()
 
@@ -24,8 +27,8 @@ if args.model is None:
 # checkpoint_path = "Qwen/Qwen2.5-Coder-1.5B-Instruct"
 checkpoint_path = env.models_folder + ("/finetunes/" if args.finetune else "/") + args.model
 llm = LLM(model=checkpoint_path)
-tokenizer = AutoTokenizer.from_pretrained(checkpoint_path)
-sampling_params = SamplingParams(temperature=0.7, top_p=0.8, repetition_penalty=1.05, max_tokens=4096)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint_path, cpu_offload_gb=args.cpu_offload_gb)
+sampling_params = SamplingParams(temperature=args.t, top_p=args.top_p, max_tokens=4096)
 
 test_dataset: Dataset = load_dataset("pvharmo/llm-gha", token=env.hf_access_token)["test"]
 
@@ -33,38 +36,32 @@ example_per_level = 200
 unique_ids = list(set(test_dataset["id"]))[:200]
 test_dataset = test_dataset.filter(lambda example: example["id"] in unique_ids)
 
-test_dataset_level1 = test_dataset.filter(lambda example: example["level"] == "level1").select(range(example_per_level))
-test_dataset_level2 = test_dataset.filter(lambda example: example["level"] == "level2").select(range(example_per_level))
-test_dataset_level3 = test_dataset.filter(lambda example: example["level"] == "level3").select(range(example_per_level))
-test_dataset_level4 = test_dataset.filter(lambda example: example["level"] == "level4").select(range(example_per_level))
-test_dataset_level5 = test_dataset.filter(lambda example: example["level"] == "level5").select(range(example_per_level))
+for level in ["level1", "level2", "level3", "level4", "level5"]:
+    dataset = test_dataset.filter(lambda example: example["level"] == level).select(range(example_per_level))
+    dataset = dataset.map(lambda example: {"tokens":tokenizer.apply_chat_template(
+        [
+            {"role": "system", "content": example["system_prompt"]},
+            {"role": "user", "content": example["user_prompt"]}
+        ],
+        tokenize=False,
+        add_generation_prompt=True
+    )})
 
-test_dataset = concatenate_datasets([test_dataset_level1, test_dataset_level2, test_dataset_level3, test_dataset_level4, test_dataset_level5])
+    results_path = f"{env.results_folder}/{args.model.replace('/','_')}-t{args.t}-top_p{args.top_p}{'-finetune' if args.finetune else ''}.jsonl"
 
-test_dataset = test_dataset.map(lambda example: {"tokens":tokenizer.apply_chat_template(
-    [
-        {"role": "system", "content": example["system_prompt"]},
-        {"role": "user", "content": example["user_prompt"]}
-    ],
-    tokenize=False,
-    add_generation_prompt=True
-)})
+    if os.path.exists(results_path):
+        os.remove(results_path)
 
-results_path = f"{env.results_folder}/{args.model.replace('/','_')}.jsonl"
+    outputs = llm.generate(dataset["tokens"], sampling_params, use_tqdm=True)
 
-if os.path.exists(results_path):
-    os.remove(results_path)
+    with open(results_path, "a") as f:
+        for example, output in tqdm(zip(test_dataset, outputs)):
+            json_line = json.dumps({
+                "id": example["id"],
+                "level": example["level"],
+                "llm_response": output.outputs[0].text,
+                "answer": example["answer"]
+            })
 
-outputs = llm.generate(test_dataset["tokens"], sampling_params)
-
-with open(results_path, "a") as f:
-    for example, output in tqdm(zip(test_dataset, outputs)):
-        json_line = json.dumps({
-            "id": example["id"],
-            "level": example["level"],
-            "llm_response": output,
-            "answer": example["answer"]
-        })
-
-        f.write(json_line)
-        f.write("\n")
+            f.write(json_line)
+            f.write("\n")
--- scripts/llama3.1-8b_finetune.sh
+++ scripts/llama3.1-8b_finetune.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --gres=gpu:2       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-6:00
-#SBATCH --output=codellama_%N-%j.out
+#SBATCH --time=0-23:00
+#SBATCH --output=%N-%j.out
 
 module load python/3.12.4 scipy-stack gcc arrow/17.0.0 cuda cudnn
 
 source /home/pvharmo/llm-gha-eval/venv/bin/activate
+mkdir $SLURM_TMPDIR/data
+mkdir -p /home/pvharmo/scratch/Llama3.1-8B-Instruct/checkpoints
 
-cd /home/pvharmo/llm-gha-eval/eval/
-python inference_vllm.py --model CodeLlama-7b-Instruct-hf -t 0.5
+cd /home/pvharmo/llm-gha-eval/finetuning
+accelerate launch llama_finetune.py --model 8b
