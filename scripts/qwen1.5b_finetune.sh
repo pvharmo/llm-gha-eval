0f9dd4d15ffa9a8bead1b24a8b6a9a495885623d
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2024-11-21 12:03:24 -0500

    change number of examples for qwen 1.5b finetuning

--- scripts/qwen1.5b_finetune.sh
+++ scripts/qwen1.5b_finetune.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-16:00
 #SBATCH --output=finetune_qwen-1.5_%N-%j.out
-#SBATCH --array=1-3
+#SBATCH --array=1,5,10,30,60,90
 #SBATCH --mail-user=jonathan.caron-roberge.1@ulaval.ca
 #SBATCH --mail-type=ALL
 
@@ -14,4 +14,4 @@ source /home/pvharmo/llm-gha-eval/venv/bin/activate
 mkdir -p /home/pvharmo/scratch/Qwen2.5-Coder-1.5B-Instruct/checkpoints
 
 cd /home/pvharmo/llm-gha-eval/finetuning
-python finetune.py --model Qwen2.5-Coder-1.5B-Instruct --nb_examples $((1000 * $SLURM_ARRAY_TASK_ID)) --epochs 1
+python finetune.py --model Qwen2.5-Coder-1.5B-Instruct --nb_examples $((100 * $SLURM_ARRAY_TASK_ID)) --epochs 1
