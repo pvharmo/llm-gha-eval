e48ec0fd51660a1c7b3ee89bb95107c1b1569134
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2024-11-22 18:29:27 -0500

    Test finetuning with 10000 examples and 1 and 2 epochs

--- scripts/qwen1.5b_inference_finetune.sh
+++ scripts/qwen1.5b_inference_finetune.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-6:00
 #SBATCH --output=inference_finetune_qwen1.5b_%A_%N-%j.out
-#SBATCH --array=1,5,10,30,55,85
+#SBATCH --array=1,2
 #SBATCH --mail-user=jonathan.caron-roberge.1@ulaval.ca
 #SBATCH --mail-type=ALL
 
@@ -13,4 +13,4 @@ module load python/3.12.4 scipy-stack gcc arrow/17.0.0 cuda cudnn
 source /home/pvharmo/projects/def-masai45/pvharmo/venv/bin/activate
 
 cd /home/pvharmo/llm-gha-eval/eval/
-python inference_vllm.py --model Qwen2.5-Coder-1.5B-Instruct --cpu-offload-gb 0 --finetune e1-n$((1000 * $SLURM_ARRAY_TASK_ID))
+python inference_vllm.py --model Qwen2.5-Coder-1.5B-Instruct --cpu-offload-gb 0 --finetune e$SLURM_ARRAY_TASK_ID-nAll
