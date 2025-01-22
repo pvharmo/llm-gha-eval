53c9691b86332eeb569e8e3b2d6c5d3cb4ff8bd7
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2025-01-22 15:49:35 -0500

    Add finetune name to codellama inference script

--- scripts/codellama_inference_ft.sh
+++ scripts/codellama_inference_ft.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-6:00
-#SBATCH --output=inference_codellama_%N-%j.out
+#SBATCH --output=inference_codellama_ft_%N-%j.out
 #SBATCH --mail-user=jonathan.caron-roberge.1@ulaval.ca
 #SBATCH --mail-type=ALL
 
@@ -12,4 +12,4 @@ module load python/3.12.4 scipy-stack gcc arrow/17.0.0 cuda cudnn
 source /home/pvharmo/projects/def-masai45/pvharmo/venv/bin/activate
 
 cd /home/pvharmo/llm-gha-eval/eval/
-python inference_vllm.py --model CodeLlama-7b-Instruct-hf
+python inference_vllm.py --model CodeLlama-7b-Instruct-hf --finetune final-checkpoint
