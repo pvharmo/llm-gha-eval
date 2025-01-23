527db570f4f977cd94f144b46495a2553280bda8
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2025-01-23 12:29:22 -0500

    Add merge finetune to base model utility script

---
+++ finetuning/merge_model.py
@@ -0,0 +1,21 @@
+import sys
+
+sys.path.append('../')
+
+import torch
+from transformers import AutoModelForCausalLM
+from peft import PeftModel
+import argparse
+
+import env
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str)
+parser.add_argument("--finetune", type=str, default=None)
+args = parser.parse_args()
+
+base_model = AutoModelForCausalLM.from_pretrained(env.models_folder + "/" + args.model, torch_dtype=torch.float16, device_map="auto")
+
+model_to_merge = PeftModel.from_pretrained(base_model.to("cuda"), env.models_folder + "/finetunes/" + args.model + "/" + args.finetune)
+merged_model = model_to_merge.merge_and_unload()
+merged_model.save_pretrained(env.models_folder + "/" + args.model + "_ft_merged")
