f50763ed8bcda371243d2c3300d79ea5b4d41ab9
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2025-02-17 13:03:38 -0500

    Update codellama finetuning script

--- scripts/codellama_finetune.sh
+++ scripts/codellama_finetune.sh
@@ -10,7 +10,6 @@
 module load python/3.12.4 scipy-stack gcc arrow/17.0.0 cuda cudnn
 
 source /home/pvharmo/projects/def-masai45/pvharmo/venv/bin/activate
-mkdir -p /home/pvharmo/scratch/CodeLlama-7b-Instruct-hf/checkpoints
 
 cd /home/pvharmo/llm-gha-eval/finetuning/
-python finetune.py --model CodeLlama-7b-Instruct-hf --nb_examples 5000 --epochs 1
+python finetune.py --model CodeLlama-7b-Instruct-hf --epochs 2
