a69128a9d0a7a84fdb7d7e591406414f16807db1
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2025-02-26 10:27:02 -0500

    Add scripts to prepare finetuning of corrector

--- scripts/qwen0.5b_finetune_prepare_corrector_dataset.sh
+++ scripts/qwen0.5b_finetune_prepare_corrector_dataset.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --mem=16000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-6:00
-#SBATCH --output=finetune_prepare_corrector_dataset.5b_%A_%N-%j.out
+#SBATCH --output=finetune_prepare_corrector_dataset0.5b_%A_%N-%j.out
 #SBATCH --mail-user=jonathan.caron-roberge.1@ulaval.ca
 #SBATCH --mail-type=ALL
 
@@ -12,4 +12,4 @@ module load python/3.12.4 scipy-stack gcc arrow/17.0.0 cuda cudnn
 source /home/pvharmo/projects/def-masai45/pvharmo/venv/bin/activate
 
 cd /home/pvharmo/llm-gha-eval/finetuning/
-python prepare_corrector_dataset.py --model Qwen2.5-Coder-1.5B-Instruct --cpu-offload-gb 0 --finetune e1-nAll
+python inference_vllm.py --model Qwen2.5-Coder-0.5B-Instruct --cpu-offload-gb 0 --finetune e2-nAll --n None --split corrector_train
--- scripts/qwen0.5b_inference_finetune.sh
+++ scripts/qwen0.5b_inference_finetune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --mem=16000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-6:00
 #SBATCH --output=inference_finetune_qwen0.5b_%A_%N-%j.out
 #SBATCH --array=1,2
